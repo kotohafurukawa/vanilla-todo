cfdda84ab9737dc83bc1efe25a60cca4345c0430
kotohafurukawa <kotoha.furukawa00@gmail.com>
2023-05-07 14:05:13 +0900

    プロパティ管理を一元化

--- js/todo_list.js
+++ js/todo_list.js
@@ -1,81 +1,126 @@
-// todo連想配列childBtnElement
+// todo連想配列
 const unCompletedTodos = {};
-// 実施済のtodo連想配列
-const doneTodos = {};
-// 追加のform要素を取得
-const addTodoBtn = document.querySelector('#addTodo').lastElementChild;
-// todoのDOMツリーを取得
-const todo = document.querySelector('#todo');
-// 実施済みのDOMツリーを取得
-const doneTodo = document.querySelector('#doneTodo');
+// todoのNodeを取得
+const unCompletedTodoNode = document.querySelector('#unCompletedTodo');
+// 実施済みのNodeを取得
+const doneTodoNode = document.querySelector('#doneTodo');
+// 追加ボタンのNodeを取得
+const addBtn = document.querySelector('#addBtn');
 //ユーザーが追加ボタンを押した回数。押下時にカウンターを増加させて、IDを一意に割り振る。
 let addCounter = 0;
+// todoを新規追加した際、発生するイベント。
+addBtn.addEventListener('click', () => userInputEvent());
 
-// ユーザーの入力値を配列に追加
+// addBtnがクリックされたとき、最初に発生する関数。
 function userInputEvent () {
   // ユーザーの入力値を取得
-  let userInput = addTodo.querySelector('input[type="text"]').value;
+  let inputTxt = document.querySelector('#inputTxt');
   // 入力値の空文字判定する
-  if(userInput === '') {
+  if(inputTxt.value === '') {
     alert('入力して下さい');
   }else {
-    unCompletedTodos[`js-todo${addCounter}`] = userInput;
+    // valueを未実施object(unCompetedTodos)に渡す。
+    unCompletedTodos[`js-todo${addCounter}`] = inputTxt.value;
+    // valueに空文字を渡す。
+    inputTxt.value = '';
     // 新たに追加された要素を表示
-    displayAddItem(unCompletedTodos, `js-todo${addCounter}`, todo);
+    displayAddItem(unCompletedTodos, `js-todo${addCounter}`, unCompletedTodoNode);
+    // カウンターをインクリメントする。
     addCounter++;
   }
 }
-// 連想配列に追加されたtodoを先頭に表示させる。
-function displayAddItem (obj, property, domTree) {
-  // 追加するDOMツリーを生成
-  // 追加するノードの一番上の親要素
-  let parentElement = document.createElement('li');
-  parentElement.className = 'c-todoList--item';
+// 連想配列に追加されたプロパティを表示する関数
+function displayAddItem (obj, propertyName, parentNode) {
+  // 追加する子ノードを作成
+  let childNode = createLiTag();
 
   // チェックボタンを生成
-  let childCheckElement = document.createElement('label');
-  childCheckElement.className = 'c-todoList--left c-checkBtn';
+  let label = createCheckBtn();
+  label.firstElementChild.id = propertyName;
+  // チェックボックスをクリックしたときのイベント
+  label.firstElementChild.addEventListener('click', () => {
+     // 現在いる親ノードを特定
+    let currentParentNode = childNode.parentNode;
+    // 親ノードの切り替えを行うう条件分岐
+    if(currentParentNode === unCompletedTodoNode) {
+      deleteNode(unCompletedTodoNode, childNode);
+      addNode(doneTodoNode, childNode);
+      // 親ノードを実施済みに切り替え
+      currentParentNode === doneTodoNode;
+    }else {
+      deleteNode(doneTodoNode, childNode);
+      addNode(unCompletedTodoNode, childNode);
+      // 親ノードを未実施に切り替え
+      currentParentNode === unCompletedTodoNode;
+    }
+  });
+
+  // pタグを生成
+  let p = createPTag();
+  console.log(p);
+  p.textContent = obj[propertyName];
+  
+  // 削除ボタンを生成
+  let button = deleteBtn();
+  // ボタンを押したときの、イベント
+  button.addEventListener('click', () => {
+    // 現在いる親ノードを特定
+    let currentParentNode = childNode.parentNode;
+    deleteProperty (obj, propertyName);
+    deleteNode (currentParentNode, childNode);
+  });
+  
+  // 子ノードに孫要素を追加する。
+  childNode.appendChild(label);
+  childNode.appendChild(p);
+  childNode.appendChild(button);
+  
+  // 親ノードの先頭に追加
+  parentNode.prepend(childNode);
+}
+// オブジェクトから対象のプロパティを削除
+function deleteProperty (targetObj, targetProperty) {
+  delete targetObj[targetProperty];
+}
+// 親ノードから対象ノードを削除
+function deleteNode (parentNode, deleteNode) {
+  parentNode.removeChild(deleteNode);
+}
+// 親ノードに対象ノードを追加
+function addNode (parentNode, addNode) {
+  parentNode.prepend(addNode);
+}
+// liタグの生成
+function createLiTag () {
+  let li = document.createElement('li');
+  li.className = 'c-todoList--item';
+
+  return li;
+}
+// チェックボタンの生成
+function createCheckBtn () {
+  let label = document.createElement('label');
+  label.className = 'c-todoList--left c-checkBtn';
   let input = document.createElement('input');
   input.type = 'checkbox';
-  input.id = property;
   let span = document.createElement('span');
-  childCheckElement.appendChild(input);
-  childCheckElement.appendChild(span);
+  label.appendChild(input);
+  label.appendChild(span);
 
-  // テキストを生成
-  let childTxtElement = document.createElement('p');
-  childTxtElement.className ='c-todoList--center';
-  childTxtElement.textContent = obj[property];
-
-  // 削除ボタンを生成
-  let childBtnElement = document.createElement('button');
-  childBtnElement.className = 'c-todoList--right c-btn';
-  childBtnElement.textContent = '削除';
-
-  // 親要素に追加していく
-  parentElement.appendChild(childCheckElement);
-  parentElement.appendChild(childTxtElement);
-  parentElement.appendChild(childBtnElement);
-
-  // 未実施の先頭に追加
-  domTree.prepend(parentElement);
+  return label;
 }
-// チェックボタン押下時に、未実施→実施済みにデータの移動あるいはその逆。
-// 追加された連想配列は、DisplayAddItem関数を実行する。
-function handleTodoChange (addObject, removeObject, addDom, removeDom, targetProperty) {
-  addObject[targetProperty.id] = removeObject[targetProperty.id];
-  let targetNode = targetProperty.closest('.c-todoList--item');
-  removeDom.removeChild(targetNode);
-  delete removeObject[targetProperty.id];
-  displayAddItem(addObject, targetProperty.id, addDom);
+// Pタグを生成
+function createPTag () {
+  let p = document.createElement('p');
+  p.className ='c-todoList--center';
+
+  return p;
 }
-// todoを新規追加した際、発生するイベント。
-addTodoBtn.addEventListener('click', userInputEvent);
-// 未実施のtodoにチェックをつけた際、発火するイベント
-todo.addEventListener('change', function(event) {
-  handleTodoChange(doneTodos, unCompletedTodos, doneTodo, todo, event.target);
-});
-// 実施済みのtodoにチェックをつけた際、発火するイベント
-doneTodo.addEventListener('change', function(event) {
-  handleTodoChange(unCompletedTodos, doneTodos, todo, doneTodo, event.target);
-});
+// 削除ボタンを生成
+function deleteBtn () {
+  let button = document.createElement('button');
+  button.className = `c-todoList--right c-btn`;
+  button.textContent = '削除';
+
+  return button;
+}
